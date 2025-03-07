0b24a0a0f1b070b463d0c33e7666a4482ee96cde
Ivan Gillig <ivan.gillig@gmail.com>
2025-03-06 23:27:57 -0300

    cors update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,15 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(cors());
+
+// CORS configuration
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 connectDB();
