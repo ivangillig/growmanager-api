c1b24ce4642c9d33ad291538da1d158bec495b80
Ivan Gillig <ivan.gillig@gmail.com>
2025-03-13 18:34:54 -0300

    batch log model created

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -69,7 +69,6 @@ export const authenticateUser = async (req, res, next) => {
 export const authorizeRoles = (...allowedRoles) => {
   return (req, res, next) => {
     const userRole = req.user && req.user.role
-    console.log(req.user)
     const isAuthorized = allowedRoles.some((role) =>
       Array.isArray(role) ? role.includes(userRole) : role === userRole
     )
---
+++ src/models/BatchLog.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose')
+
+const BatchLogSchema = new mongoose.Schema(
+  {
+    batchId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Batch',
+      required: true,
+    },
+    interventionDate: {
+      type: Date,
+      required: true,
+    },
+    plantHeight: {
+      type: Number,
+      required: false,
+    },
+    relativeHumidity: {
+      type: Number,
+      required: false,
+    },
+    soilHumidity: {
+      type: Number,
+      required: false,
+    },
+    temperature: {
+      type: Number,
+      required: false,
+    },
+    ph: {
+      type: Number,
+      required: false,
+    },
+    interventions: {
+      type: [String],
+      enum: [
+        'fertilization',
+        'pesticides',
+        'pruning',
+        'defoliation',
+        'training',
+      ],
+      required: false,
+    },
+    fertilizerType: {
+      type: String,
+      required: false,
+    },
+    fertilizerDose: {
+      type: Number,
+      required: false,
+    },
+    pesticideType: {
+      type: String,
+      required: false,
+    },
+    pesticideDose: {
+      type: Number,
+      required: false,
+    },
+    pruningType: {
+      type: String,
+      enum: ['topping', 'fimming', 'lollipopping'],
+      required: false,
+    },
+    trainingTechnique: {
+      type: String,
+      enum: ['lst', 'hst', 'scrog', 'sog'],
+      required: false,
+    },
+    observations: {
+      type: String,
+      maxlength: 500,
+      required: false,
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('BatchLog', BatchLogSchema)
